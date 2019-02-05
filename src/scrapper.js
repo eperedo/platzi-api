259c6fa4a49b0e658ea1f5ff49da6d7182ed6f40
Eduardo P. Rivero <jrperedo@gmail.com>
2019-02-05 15:39:11 -0500

    feat(api): Added profile endpoint

---
+++ src/scrapper.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+const urlBase = 'https://platzi.com';
+
+async function getProfileInfo(username) {
+	const browser = await puppeteer.launch({
+		args: chrome.args,
+		// ugly hack since we cannot load chrome-aws-lambda in local
+		executablePath:
+			process.env.NODE_ENV !== 'production'
+				? process.env.CHROME_PATH
+				: chrome.executablePath,
+		headless: chrome.headless,
+	});
+
+	const page = await browser.newPage();
+	await page.goto(`${urlBase}/@${username}`);
+	const profile = await page.evaluate(() => {
+		const isPrivate = document.querySelector('.PrivateProfile');
+		const isError = document.querySelector('.Errorpage');
+		if (isPrivate) {
+			return 1;
+		} else if (isError) {
+			return 2;
+		} else {
+			const careersDom = document.querySelectorAll('.Career-flex');
+			const profileInfo = {
+				careers: [],
+				courses: [],
+			};
+			careersDom.forEach(career => {
+				const badge = career.querySelector('img').src;
+				const title = career.querySelector('.Career-title').textContent;
+				profileInfo.careers.push({ badge, title });
+			});
+			return profileInfo;
+		}
+	});
+	await browser.close();
+	return profile;
+}
+
+module.exports = getProfileInfo;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const scrapper = require('./scrapper');
 
 const server = new Hapi.Server({
 	host: '0.0.0.0',
@@ -8,9 +9,20 @@ const server = new Hapi.Server({
 });
 
 server.route({
-	async handler(request) {
+	async handler(request, h) {
 		const { username } = request.params;
-		return username;
+		if (username) {
+			const result = await scrapper(username);
+			if (typeof result === 'number') {
+				if (result === 1) {
+					return h.response().code(403);
+				} else if (result === 2) {
+					return h.response().code(404);
+				}
+			}
+			return h.response(result);
+		}
+		return h.response().code(400);
 	},
 	method: 'GET',
 	path: '/profile/{username}',
