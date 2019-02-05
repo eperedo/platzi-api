fddad6267d05eef9331f21e7f2df5b0c74d3202f
Eduardo P. Rivero <jrperedo@gmail.com>
2019-02-05 16:59:14 -0500

    feat(api): Added personal information, points and old courses

--- src/scrapper.js
+++ src/scrapper.js
@@ -21,15 +21,47 @@ async function getProfileInfo(username) {
 		} else if (isError) {
 			return 2;
 		} else {
+			const facebookDom = document.querySelector('.icon-fcbk');
+			const twitterDom = document.querySelector('.icon-twt');
+			const flagDom = document.querySelector('.ProfileHeader-flag');
+			const websiteDom = document.querySelector('.UserUrl-link');
+			const bioDom = document.querySelector('.Biography-text');
+			const nameDom = document.querySelector('.ProfileHeader-name');
 			const careersDom = document.querySelectorAll('.Career-flex');
+			const coursesDom = document.querySelectorAll('.Course.is-principal');
+			const pointsDom = document.querySelectorAll('.ProfileScore-number');
+			const oldCoursesDom = document.querySelectorAll('.Course.is-deprecated');
 			const profileInfo = {
+				bioDom: bioDom ? bioDom.textContent : '',
 				careers: [],
+				country: flagDom ? flagDom.alt : '',
 				courses: [],
+				facebook: facebookDom ? facebookDom.href : '',
+				flagUrl: flagDom ? flagDom.src : '',
+				name: nameDom ? nameDom.textContent : '',
+				oldCourses: [],
+				totalPoints: pointsDom[0] ? Number(pointsDom[0].textContent) : 0,
+				totalQuestions: pointsDom[1] ? Number(pointsDom[1].textContent) : 0,
+				totalAnswers: pointsDom[2] ? Number(pointsDom[2].textContent) : 0,
+				twitter: twitterDom ? twitterDom.href : '',
+				website: websiteDom ? websiteDom.href : '',
 			};
 			careersDom.forEach(career => {
 				const badge = career.querySelector('img').src;
 				const title = career.querySelector('.Career-title').textContent;
 				profileInfo.careers.push({ badge, title });
+			});
+			coursesDom.forEach(course => {
+				const badge = course.querySelector('img').src;
+				const title = course.querySelector('.Course-title').textContent;
+				const career = course.querySelector('.Course-career').textContent;
+				profileInfo.courses.push({ badge, title, career });
+			});
+			oldCoursesDom.forEach(course => {
+				const badge = course.querySelector('img').src;
+				const title = course.querySelector('.Course-title.is-deprecated')
+					.textContent;
+				profileInfo.oldCourses.push({ badge, title });
 			});
 			return profileInfo;
 		}
